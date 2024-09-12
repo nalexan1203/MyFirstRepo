6b1931c8e2539f8a163d15c7f25a0afa80f70bb8
nalexan1203 <nikosalexandris1990@gmail.com>
2024-09-12 12:01:36 +0300

    While Update

--- practise.py
+++ practise.py
@@ -1,4 +1,31 @@
-from modulo import get_phone
 
-value = get_phone(1, 123, 456, 7890)
-print(value.replace('-',''))
+ 
+menu = {"pizza" : 3.00,
+        "nachos" : 4.50,
+        "popcorn" : 6.00,
+        "fries" : 2.50,
+        "chips" : 1.00
+}
+
+
+cart = []
+total = 0
+
+while True:
+    food = input(f"Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+for food in cart:
+    print(food, end=" ")
+
+for value in cart:
+    total += menu.get(value)
+
+print( )  
+print(f"{total}")
+
+
+input()
