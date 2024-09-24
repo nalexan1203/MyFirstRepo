a9254b8cd724bdd3a09e6e5a21f5901f2e690950
nalexan1203 <nikosalexandris1990@gmail.com>
2024-09-24 21:08:01 +0300

    Update

--- RestAPI.py
+++ RestAPI.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 base_url = "https://jsonplaceholder.typicode.com/users"
 
@@ -6,16 +7,17 @@ base_url = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(base_url)
 
 print(type(response))
-#Attribute of the Response Object
 
+
+#Attribute of the Response Object
 if response.status_code == 200: 
     datas = response.json()
+    print(type(datas))
 
-for data in datas:
+print(datas[0])
+
+'''for data in datas:
     if data.get("id") == 1:
-        print(data["name"])
+        print(data["name"])'''
 
 
-mitsos = "Nikos"
-for x in mitsos:
-    print(x, end="")
