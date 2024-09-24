52e91880f1457e5c5e485a0c57bdea74d8074354
nalexan1203 <nikosalexandris1990@gmail.com>
2024-09-24 20:28:39 +0300

    Udpate

---
+++ Function.py
@@ -0,0 +1,3 @@
+total = []
+
+print(help(total))
---
+++ RestAPI.py
@@ -0,0 +1,16 @@
+import requests
+
+
+base_url = "https://jsonplaceholder.typicode.com/users"
+
+#Response Object
+response = requests.get(base_url)
+
+print(type(response))
+#Attribute of the Response Object
+if response.status_code == 200: 
+    datas = response.json()
+
+for data in datas:
+    if data.get("id") == 1:
+        print(data["name"])
